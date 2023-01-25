d61cc924b2701e29522834213dee67bd56402c42
Krzysztof Krukowski <krzysztof.krukowski95@gmail.com>
2023-01-26 00:57:11 +0100

    with themes handling

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 const rootElement = document.getElementById("root");
 const ReactRoot = ReactDOM.createRoot(rootElement!);
 
 ReactRoot.render(
   <HelmetProvider>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </HelmetProvider>
 );
