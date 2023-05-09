6507f03a51166edabb6b4e252e05d4ed3682bda6
Krzysztof Krukowski <krzysztof.krukowski95@gmail.com>
2023-05-09 16:58:25 +0200

    new version of Tooltip component

--- src/design-system/components/Tooltip/Tooltip.tsx
+++ src/design-system/components/Tooltip/Tooltip.tsx
@@ -1,21 +1,26 @@
 import React from "react";
-import "@design-system/design-system.scss";
 
 type TooltipProps = {
   children: React.ReactNode;
-  tooltipInset: React.ReactNode;
+  tooltipInset?: React.ReactNode;
   position?: "top" | "bottom" | "left" | "right";
+  className?: string;
 };
 
-const Tooltip = ({ children, tooltipInset, position = "top" }: TooltipProps) => {
+const Tooltip = ({ children, tooltipInset, position = "top", className }: TooltipProps) => {
   const [show, setShow] = React.useState(false);
 
   const handleMouseEnter = () => setShow(true);
   const handleMouseLeave = () => setShow(false);
 
   return (
-    <div className="ds-tooltip-wrapper" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      {show && <div className={`ds-tooltip ds-tooltip-${position}`}>{tooltipInset}</div>}
+    <div className={`ds-tooltip-wrapper ${className}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      {show && tooltipInset && (
+        <div className={`ds-tooltip ds-tooltip-${position}`}>
+          {tooltipInset}
+          <div className="ds-tooltip-triangle" />
+        </div>
+      )}
       <div className="ds-tooltip-children">{children}</div>
     </div>
   );
